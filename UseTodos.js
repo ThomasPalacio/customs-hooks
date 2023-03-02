5a681f87e1dc02aa05be2468d094c725e260efa4
Thomas Palacio <palaciothomas007@gmail.com>
2023-03-02 17:00:46 -0300

    Primer commit

---
+++ UseTodos.js
@@ -0,0 +1,44 @@
+import { todoReducer } from "../08-useReducer/todoReducer"
+import { useReducer,useEffect } from "react"
+
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos'))|| [];
+    }
+
+export const UseTodos = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, [], init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    
+    }, [todos])
+        
+    const handleNewTodo = (todo) => {
+        const action = {
+            type:'Add Todo',
+            payload: todo
+        }
+    
+        dispatch(action);
+    }
+    
+    const handleDeleteTodo = (id) =>{
+         dispatch({
+            type:'Remove Todo',
+            payload: id
+        });
+     
+    }
+    
+    const handleToggleTodo = (id) =>{
+        dispatch({
+            type:'Toggle Todo',
+            payload: id
+        });
+    }
+
+    return { handleToggleTodo, handleNewTodo,handleDeleteTodo,todos, todosCount: todos.length, pendingTodosCount: todos.filter(todo => !todo.done).length}
+}
